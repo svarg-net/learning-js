6f1d4db93a802ec3239b743abeebb199c3a6c7da
svarg-net <svarg.net@yandex.ru>
2021-11-01 18:21:15 +0500

    task: Fix string case

---
+++ 7k/task-fix-string-case/task.js
@@ -0,0 +1,13 @@
+function solve(s){
+    let iUp=[];
+    let iLow=[];
+    let i=0;
+    for(i; i<s.length;i++)
+        s[i]==s[i].toUpperCase()?iUp.push(i):iLow.push(i)
+    console.log(iUp);
+    console.log(iLow);
+    let arr=s.split("");
+    iUp.length<=iLow.length?iUp.map((i)=>{arr.splice(i,1,s[i].toLowerCase())}):iLow.map((i)=>{arr.splice(i,1,s[i].toUpperCase())})
+    // console.log(s.replaceAt(3,s[i].toUpperCase()))
+    return arr.join("");
+}
